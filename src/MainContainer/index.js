e9001fddccb038505a8f1460346f5df1b5ecbf16
seheesf88 <seheesf88@gmail.com>
2019-06-08 12:40:44 -0700

    example

---
+++ src/MainContainer/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class MainContainer extends Component {
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  }
+
+
+  render(){
+    return (
+      <div>
+        <form>
+          name: <input />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default MainContainer
